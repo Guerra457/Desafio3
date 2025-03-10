e09a61a2a679d1a63ff07df42e4fb2e164d2a906
Matheus Guerra <matheus4xp@gmail.com>
2025-03-10 20:58:32 -0300

    Criação do Projeto

---
+++ ms-event-manager/src/main/java/org/compass/mseventmanagerapi/MsEventManagerApiApplication.java
@@ -0,0 +1,13 @@
+package org.compass.mseventmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEventManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEventManagerApiApplication.class, args);
+    }
+
+}
---
+++ ms-ticket-manager/src/main/java/org/compass/msticketmanagerapi/MsTicketManagerApiApplication.java
@@ -0,0 +1,13 @@
+package org.compass.msticketmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTicketManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTicketManagerApiApplication.class, args);
+    }
+
+}
